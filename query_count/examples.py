0204ded2525e9f55291341fd720c253e0b54b240
Stephen Burrows <stephen.r.burrows@gmail.com>
2017-08-26 15:21:33 +0200

    Added examples for prefetch_related

--- query_count/examples.py
+++ query_count/examples.py
@@ -1,3 +1,6 @@
+import random
+
+from django.db.models import Prefetch
 from faker import Faker
 
 from query_count.models import (
@@ -21,13 +24,19 @@ fake = Faker()
 
 
 def create_blog_posts(count):
-    blog = Blog.objects.create(name=fake.words(2))
+    blog = Blog.objects.create(name=' '.join(fake.words(2)))
+    Tag.objects.bulk_create((
+        Tag(name=' '.join(fake.words(2)))
+        for x in range(50)
+    ))
+    tags = list(Tag.objects.all())
     for x in range(count):
-        BlogPost.objects.create(
-            title=fake.words(5),
+        post = BlogPost.objects.create(
+            title=' '.join(fake.words(5)),
             blog=blog,
             author=Author.objects.create(name=fake.name()),
         )
+        post.tags = random.sample(tags, random.randint(1, 5))
 
 
 # Rollback and count_queries are small utility decorators that ensure
@@ -67,3 +76,51 @@ def following_foreignkeys__large__select_related():
     with count_queries():
         for blog_post in BlogPost.objects.select_related('author'):
             print(blog_post.author.name)
+
+
+@rollback
+def following_m2m():
+    create_blog_posts(300)
+
+    with count_queries():
+        for blog_post in BlogPost.objects.all():
+            print(', '.join((tag.name for tag in blog_post.tags.all())))
+
+
+@rollback
+def following_m2m__prefetch_related():
+    create_blog_posts(300)
+
+    with count_queries():
+        for blog_post in BlogPost.objects.prefetch_related('tags'):
+            print(', '.join((tag.name for tag in blog_post.tags.all())))
+
+
+@rollback
+def following_reverse_fk():
+    create_blog_posts(300)
+
+    with count_queries():
+        for author in Author.objects.all():
+            print(author.name)
+
+            for blog_post in author.blogpost_set.all():
+                print(blog_post.title, '||', blog_post.blog.name)
+
+
+@rollback
+def following_reverse_fk__prefetch_related__select_related():
+    create_blog_posts(300)
+
+    with count_queries():
+        authors = Author.objects.prefetch_related(
+            Prefetch(
+                'blogpost_set',
+                queryset=BlogPost.objects.select_related('blog'),
+            )
+        )
+        for author in authors:
+            print(author.name)
+
+            for blog_post in author.blogpost_set.all():
+                print(blog_post.title, '||', blog_post.blog.name)
